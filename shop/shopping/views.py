48da02a1f6d045ef87cb16a4ec7cfb25462c70b1
AIM1993 <244018719@qq.com>
2018-09-23 11:34:56 +0800

    完善了部分urls

--- shop/shopping/views.py
+++ shop/shopping/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import 产品列表, 商品类别表
 # Create your views here.
+
+
+def home(request):
+    content = {'所有商品': 产品列表.objects.all()}
+    return render(request, 'shopping/home.html', content)
