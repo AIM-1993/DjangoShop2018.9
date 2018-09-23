3ee089dca9215f1ccb34043ec7ba2d7cfb66bb2d
AIM1993 <244018719@qq.com>
2018-09-23 20:52:12 +0800

    继续完善views.py

--- shop/shopping/views.py
+++ shop/shopping/views.py
@@ -7,6 +7,6 @@ def home(request):
     Cate_data = Category.objects.all()
     Cate_info = []
     for cate in Cate_data:
-        Cate_info.append(category, Product.objects.filter(cate=cate)[:3])
+        Cate_info.append(Category, Product.objects.filter(cate=cate)[:3])
     content = {'Cate_info': Cate_info}
     return render(request, 'shopping/home.html', content)
