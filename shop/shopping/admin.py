c6f8c63c8a1d26a9a925278902e687cd446b2c17
AIM1993 <244018719@qq.com>
2018-09-23 09:48:52 +0800

    初始化前端工作

--- shop/shopping/admin.py
+++ shop/shopping/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import 产品列表, 商品类别表
 # Register your models here.
-admin.site.register(产品列表)
-admin.site.register(商品类别表)
+
+# 商品列表
+class 商品类别表Admin(admin.ModelAdmin):
+    list_display = ['id', '名称', '图片']
+
+admin.site.register(商品类别表, 商品类别表Admin)
+
+# 产品列表
+class 产品列表Admin(admin.ModelAdmin):
+    list_display = ['id', '名称', '图片', '所属类别', '价格', '库存', '上架情况', '创建时间', '修改时间']
+    list_editable = ['名称', '所属类别', '价格', '库存', '上架情况']
+    list_per_page = 10
+
+admin.site.register(产品列表, 产品列表Admin)
